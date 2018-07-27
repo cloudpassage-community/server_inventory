b8095bc83473226b5daacf2b2162c922f962ecfe
Hana Lee <hlee@cloudpassage.com>
2018-07-27 13:45:22 -0700

    ascii code

--- lib/csv_writer.py
+++ lib/csv_writer.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 import csv
 
 class CsvWriter(object):
@@ -5,13 +6,16 @@ class CsvWriter(object):
     def write(output, server_group, srvs_info):
         filename = "%s.csv" % server_group
         with open(filename, "wb") as f:
-            writer = csv.writer(f)
-            writer.writerows(output)
-            writer.writerows(srvs_info)
+            writer = csv.writer(f.encode('utf-8'))
+            writer.writerows(output.encode('utf-8'))
+            writer.writerows(srvs_info.encode('utf-8'))
 
     @staticmethod
     def append(server_group, srvs_info):
         filename = "%s.csv" %server_group
         with open(filename, "a") as f:
-            writer = csv.writer(f)
-            writer.writerows(srvs_info)
+            writer = csv.writer(f.encode('utf-8'))
+            try:
+                writer.writerows(srvs_info)
+            except UnicodeError:
+                srvs_info = srvs_info.encode('utf-8')
