7c1c705b741389438d3e108e77a26183ece390d3
Hana Lee <hlee@cloudpassage.com>
2018-08-01 13:15:06 -0700

    added econded_filename for filename

--- lib/csv_writer.py
+++ lib/csv_writer.py
@@ -5,16 +5,18 @@ class CsvWriter(object):
     @staticmethod
     def write(output, server_group, srvs_info):
         filename = "%s.csv" % server_group
-        with open(filename, "wb") as f:
-            writer = csv.writer(f.encode('utf-8'))
+        encoded_filename = filename.encode('utf-8')
+        with open(encoded_filename, "wb") as f:
+            writer = csv.writer(f)
             writer.writerows(output.encode('utf-8'))
             writer.writerows(srvs_info.encode('utf-8'))
 
     @staticmethod
     def append(server_group, srvs_info):
         filename = "%s.csv" %server_group
-        with open(filename, "a") as f:
-            writer = csv.writer(f.encode('utf-8'))
+        encoded_filename = filename.encode('utf-8')
+        with open(encoded_filename, "a") as f:
+            writer = csv.writer(f)
             try:
                 writer.writerows(srvs_info)
             except UnicodeError:
